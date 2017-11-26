e152c308d8f853d4500719589f83427c70e1f652
Amine <32715913+AmineDjeghri@users.noreply.github.com>
2017-11-26 18:25:31 +0100

    Added the project
    analyse lexicale et syntaxique ont été ajouté , il ne reste plus que l'analyse sémantique

---
+++ synt.tab.h
@@ -0,0 +1,79 @@
+
+/* A Bison parser, made by GNU Bison 2.4.1.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     mc_ALGORITHME = 258,
+     mc_type = 259,
+     mc_VAR = 260,
+     mc_DEBUT = 261,
+     mc_FIN = 262,
+     mc_Pour = 263,
+     mc_jusque = 264,
+     mc_Faire = 265,
+     mc_Fait = 266,
+     mc_SI = 267,
+     op_AFF = 268,
+     op_comp = 269,
+     op_arith = 270,
+     bar = 271,
+     parenthese_gauche = 272,
+     parenthese_droite = 273,
+     identificateur = 274,
+     constante = 275,
+     commentaire = 276,
+     dp = 277,
+     pvg = 278,
+     crochet_gauche = 279,
+     crochet_droit = 280
+   };
+#endif
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef int YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+
---
+++ synt.y
@@ -0,0 +1,81 @@
+%token  mc_ALGORITHME mc_type mc_VAR mc_DEBUT mc_FIN mc_Pour mc_jusque mc_Faire mc_Fait mc_SI op_AFF op_comp op_arith bar parenthese_gauche parenthese_droite identificateur constante commentaire dp pvg crochet_gauche crochet_droit 
+
+%%
+structure_generale:dec_algo mc_VAR partieDeclaration mc_DEBUT partieInstruction mc_FIN {printf("correcte");}
+;
+
+
+
+//-------------------------declaration de l'algorithme-----------
+dec_algo: mc_ALGORITHME identificateur 
+;
+
+//-------------Partie Declaration--------------
+partieDeclaration: dec_var2 partieDeclaration 
+						  | dec_var2	
+				  |dec_tableau partieDeclaration
+					         | dec_tableau
+				  |dec_var partieDeclaration
+						 | dec_var
+;
+
+//----------------les declarations------------
+dec_tableau: identificateur crochet_gauche constante crochet_droit dp mc_type pvg
+;
+dec_var2: mc_type ListeIDF pvg 
+;
+dec_var: identificateur dp mc_type pvg 
+;
+ListeIDF: identificateur bar ListeIDF
+			           | identificateur
+;
+					   
+//------------------PartieInstruction---------------------
+partieInstruction: inst_aff partieInstruction    
+					| inst_boucle partieInstruction 
+					| inst_cond  partieInstruction
+					| inst_aff
+					|inst_boucle
+					|inst_cond
+;					
+
+
+//-------------------Boucle------------
+inst_boucle: mc_Pour identificateur op_AFF constante mc_jusque constante mc_Faire partieInstruction mc_Fait 
+;
+
+//------------------Instruction Condition-------------
+inst_cond:mc_Faire inst_aff mc_SI parenthese_gauche cond parenthese_droite 
+;
+
+//------------------Condition-------------
+cond: identificateur op_comp constante 
+				             		     
+	 | constante op_comp constante					     	 
+;
+
+
+//------------------Instruction Affectation-----------
+inst_aff: identificateur op_AFF exp_arith pvg 
+;
+
+
+
+//------------------Expression Arithmetique---------
+exp_arith: identificateur op_arith exp_arith 
+          |constante   op_arith  exp_arith
+		  |constante
+		  |identificateur
+;
+
+
+
+%%
+main ()
+{
+printf("Taper stop pour arreter \n");
+yyparse();
+}
+yywrap()
+{
+}
